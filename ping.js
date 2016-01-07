067418979ac8f6fd812d01011ae9a6cd295d0951
Ryan Fitzgerald <ryan.fitz1@gmail.com>
2016-01-07 15:02:41 +0000

    ping url

--- ping.js
+++ ping.js
@@ -1,4 +1,39 @@
+'use strict';
+
+var request = require('request');
+
+var internals = {};
+
+internals.ping = function (url, callback) {
+  var startTime = Date.now();
+
+  request(url, function (error, response) {
+    if (error) {
+      return callback(error) ;
+    }
+
+    var elapsed = Date.now() - startTime;
+
+    var result = {url: url, latency : elapsed, statusCode: response.statusCode};
+
+    if (response.statusCode < 400) {
+      result.status = 'UP';
+    } else {
+      result.status = 'DOWN';
+    }
+
+    return callback(null, result);
+  });
+};
 
 module.exports = function (context, cb) {
-  cb(null, { hello: context.data.name || 'Anonymous' });
+  var url = context.data.url || 'https://auth0.com/';
+
+  internals.ping(url, function (err, data) {
+    if (err) {
+      return cb(err);
+    }
+
+    return cb(null, data);
+  });
 };
