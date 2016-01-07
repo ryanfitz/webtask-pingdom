335251919695f024bb95e24127f8fb57a84cb5ed
Ryan Fitzgerald <ryan.fitz1@gmail.com>
2016-01-07 15:30:31 +0000

    publish down alert

--- ping.js
+++ ping.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var request = require('request');
+var request = require('request'),
+    AWS     = require('aws-sdk');
 
 var internals = {};
 
@@ -26,12 +27,42 @@ internals.ping = function (url, callback) {
   });
 };
 
+internals.createSNSClient = function (data) {
+  AWS.config.update({
+    accessKeyId: data.KEY_ID,
+    secretAccessKey: data.SECRET_ACCESS_KEY,
+    region : data.REGION || 'us-east-1'
+  });
+
+  return new AWS.SNS();
+};
+
+internals.alertHostDown = function (sns, data) {
+  var params = {
+    TopicArn : 'arn:aws:sns:us-east-1:839951348153:NotifyMe',
+    Subject : 'Host Down Alert',
+    Message : JSON.stringify(data, null, 2)
+  };
+
+  sns.publish(params, function (err) {
+    if (err) {
+      console.log('failed to publish alert', err);
+    }
+  });
+};
+
 module.exports = function (context, cb) {
   var url = context.data.url || 'https://auth0.com/';
 
   internals.ping(url, function (err, data) {
     if (err) {
       return cb(err);
+    }
+
+    if (data.status === 'DOWN') {
+      // send alert in background ignoring errors
+      var sns = internals.createSNSClient(context.data);
+      internals.alertHostDown(sns, data);
     }
 
     return cb(null, data);
